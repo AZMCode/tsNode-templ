bba0d570396ebd3b8024004b803efa53a3cad8bd
AZMCode <15837568+AZMCode@users.noreply.github.com>
2020-07-31 10:28:55 -0400

    Updated githubify script
    Signed-off-by: AZMCode <15837568+AZMCode@users.noreply.github.com>

--- devScripts/githubifyDocs.sh
+++ devScripts/githubifyDocs.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
-#Shamelessly stolen from msspshaw at https://github.com/tgreyuk/typedoc-plugin-markdown/issues/47#issuecomment-508257371
-# Flatten docs structure
-cd docs/Markdown
-originalPaths=`find  . -mindepth 2 -type f`
-find  . -mindepth 2 -type f -exec mv {} . \;
-find . -type d -empty -delete
-
+#Idea shamelessly stolen from msspshaw at https://github.com/tgreyuk/typedoc-plugin-markdown/issues/47#issuecomment-508257371
 # Strip out .md from raw text to support Github Wiki
-sed -i '' -e 's/.md//' *
+find docs/Markdown -type f -exec sed -i '' -e 's/\.md//g' {} \;
 
-# Return to <project>
-cd ../../
 
