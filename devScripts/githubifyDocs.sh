3f93340b384d0b305c699b50d8c600226e3a1e08
AZMCode <15837568+AZMCode@users.noreply.github.com>
2020-07-31 10:32:21 -0400

    Further tweaks to the script
    Signed-off-by: AZMCode <15837568+AZMCode@users.noreply.github.com>

--- devScripts/githubifyDocs.sh
+++ devScripts/githubifyDocs.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
-#Idea shamelessly stolen from msspshaw at https://github.com/tgreyuk/typedoc-plugin-markdown/issues/47#issuecomment-508257371
+#Shamelessly stolen from msspshaw at https://github.com/tgreyuk/typedoc-plugin-markdown/issues/47#issuecomment-508257371
+
+originalPaths=`find  . -mindepth 2 -type f`
+find  . -mindepth 2 -type f -exec mv {} . \;
+find . -type d -empty -delete
+
+# Strip out folder structure from links to support Github Wiki
+while read -r line; do
+    # Remove leading ./ from each file name
+    line=` sed "s|^./||" <<< $line `
+    trimmedLine=` sed "s|.*/||" <<< $line `
+    sed -i '' -e "s|${line}|${trimmedLine}|" *
+done <<< "$originalPaths"
+
 # Strip out .md from raw text to support Github Wiki
-find docs/Markdown -type f -exec sed -i '' -e 's/\.md//g' {} \;
+find ./ -type f -exec sed -i '' -e 's/\.md//g' {} \;
 
 
